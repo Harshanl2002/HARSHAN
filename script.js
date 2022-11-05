601103860df8052922d281c781e15e416b3b3f29
unknown <harshan.l@outlook.com>
2022-11-05 18:23:07 +0530

    js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const mnbar = document.getElementById("menu");
 const mc = document.getElementById("mc");
 const l = document.getElementById("links");
-
+const lo = document.getElementById("logo");
+const n = document.getElementById("nav");
 mnbar.onclick = () => {
     mnbar.style.display = "none";
+    n.style.height = "100%";
     mc.style.display = "block";
     l.style.display = "block";
 }
 mc.onclick = () => {
     mnbar.style.display = "block";
+    n.style.height = "10%";
+    mc.style.display = "none";
+    l.style.display = "none";
+}
+
+l.onclick = () => {
+    mnbar.style.display = "block";
+    n.style.height = "10%";
     mc.style.display = "none";
     l.style.display = "none";
 }
