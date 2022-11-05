8df08227307e0c2f9f8cea3bf11b7d37423aa1b1
unknown <harshan.l@outlook.com>
2022-11-05 22:09:12 +0530

    mobile

--- script.js
+++ script.js
@@ -4,25 +4,18 @@ const l = document.getElementById("links");
 const lo = document.getElementById("logo");
 const n = document.getElementById("nav");
 var w = screen.width;
+
 mnbar.onclick = () => {
     mnbar.style.display = "none";
-    n.style.height = "100%";
     mc.style.display = "block";
-    l.style.display = "block";
+    l.style.transform = "translateX(0%)";
+    l.style.transition = "transform .5s linear"
 }
+
 mc.onclick = () => {
     mnbar.style.display = "block";
     n.style.height = "10%";
     mc.style.display = "none";
-    l.style.display = "none";
+    l.style.transform = "translateX(100%)";
+    l.style.transition = "transform .5s linear";
 }
-
-// console.log(w);
-// if (w <= 650) {
-//     l.onclick = () => {
-//         mnbar.style.display = "block";
-//         n.style.height = "10%";
-//         mc.style.display = "none";
-//         l.style.display = "none";
-//     }
-// }
