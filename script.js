9c97e224937ea0a753b576b78747c1a1388d298d
unknown <harshan.l@outlook.com>
2022-11-05 16:30:30 +0530

    js added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-var
+const mnbar = document.getElementById("menu");
+const mc = document.getElementById("mc");
+const l = document.getElementById("links");
+
+mnbar.onclick = () => {
+    mnbar.style.display = "none";
+    mc.style.display = "block";
+    l.style.display = "block";
+}
+mc.onclick = () => {
+    mnbar.style.display = "block";
+    mc.style.display = "none";
+    l.style.display = "none";
+}
