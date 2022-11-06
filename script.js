dd7dabbd0aab56a90ccf257a7dc4d2907cc1e57f
unknown <harshan.l@outlook.com>
2022-11-06 07:35:21 +0530

    nav bar ver 2.0

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 const mnbar = document.getElementById("menu");
-const mc = document.getElementById("mc");
 const l = document.getElementById("links");
-const lo = document.getElementById("logo");
-const n = document.getElementById("nav");
-var w = screen.width;
 
 mnbar.onclick = () => {
-    mnbar.style.display = "none";
-    mc.style.display = "block";
-    l.style.transform = "translateX(0%)";
-    l.style.transition = "transform .5s linear"
-}
-
-mc.onclick = () => {
-    mnbar.style.display = "block";
-    n.style.height = "10%";
-    mc.style.display = "none";
-    l.style.transform = "translateX(100%)";
-    l.style.transition = "transform .5s linear";
+    l.classList.toggle("dis");
+    mnbar.children[0].classList.toggle("t1")
+    mnbar.children[1].classList.toggle("t2")
+    mnbar.children[2].classList.toggle("t3")
 }
