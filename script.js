abe859f8a0701e91f11aab94aae9295c154cfbd8
unknown <harshan.l@outlook.com>
2022-11-06 10:05:38 +0530

    nav bar ver 2.0 mob

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const mnbar = document.getElementById("menu");
 const l = document.getElementById("links");
-
+const lo = document.getElementById("nav");
 mnbar.onclick = () => {
     l.classList.toggle("dis");
     mnbar.children[0].classList.toggle("t1")
     mnbar.children[1].classList.toggle("t2")
     mnbar.children[2].classList.toggle("t3")
+    lo.classList.toggle("dis1")
 }
